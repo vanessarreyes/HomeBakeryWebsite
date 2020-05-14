e4774909171f1de0991895765276178c28dcdaad
vanessarreyes <vanessarreyes12@gmail.com>
2020-05-14 11:09:05 -0600

    add beginning files

---
+++ public/index.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB1J6RhfDq4Z7xpJHElqxGcnj_LlBgBQHA",
+  authDomain: "anisas-cake-creations.firebaseapp.com",
+  databaseURL: "https://anisas-cake-creations.firebaseio.com",
+  projectId: "anisas-cake-creations",
+  storageBucket: "anisas-cake-creations.appspot.com",
+  messagingSenderId: "948257431971",
+  appId: "1:948257431971:web:c92d8fbd418c71b3703a16",
+  measurementId: "G-GWEYVLS8MV"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+console.log(firebase);
